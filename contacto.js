0a53dc71817b5763259ac26b2c021260e50541eb
Carlamejias <102421609+Carlamejias@users.noreply.github.com>
2022-04-30 01:59:58 -0400

    Add files via upload

---
+++ contacto.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("form")
+const nombre = document.getElementById("Nombre")
+const correo = document.getElementById("correo")
+const tel = document.getElementById("tel")
+const ciudad = document.getElementById("ciudad")
+const coment = document.getElementById("coment")
+const texto = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    texto.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += "El nombre no tiene un formato valido <br>"
+        entrar = true
+    }
+    if(!regexEmail.test(correo.value)){
+        warnings += "El correo no tiene un formato valido <br>"
+        entrar = true
+    }
+    if((tel.value.length >9 <9)){
+        warnings += "El numero telefonico no tiene un formato valido <br>"
+        entrar = true
+    }
+    if((ciudad.value.length <5)){
+        warnings += "La ciudad no tiene un formato valido <br>"
+        entrar = true
+    }
+    if((coment.value.length <30 >50)){
+        warnings += "El comentario es muy corto<br>"
+        entrar = true
+    }
+    if(entrar){
+        texto.innerHTML = warnings
+    }else{
+        texto.innerHTML = "Enviado"
+    }
+})
